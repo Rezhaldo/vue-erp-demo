32260fbfcd1dca03b151e1c35cd0b99867e70543
kevinchilmirezhaldo <rezhaldo@gmail.com>
2025-08-08 16:49:24 +0700

    initial commit

---
+++ src/modules/authentication/pages/login/ui/LoginPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login-container">
+    <h1>Login</h1>
+
+    <form @submit.prevent>
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input
+          id="username"
+          v-model="username"
+          type="text"
+          placeholder="Enter your username"
+          required
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          id="password"
+          v-model="password"
+          type="password"
+          placeholder="Enter your password"
+          required
+        />
+      </div>
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import "./LoginPage.scss"; // Import external SCSS
+
+const username = ref<string>("");
+const password = ref<string>("");
+</script>
