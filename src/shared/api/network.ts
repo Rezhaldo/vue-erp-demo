93df0225d7b2c534fa5e3c8a9806f4730aecd3d1
kevinchilmirezhaldo <rezhaldo@gmail.com>
2025-08-10 01:43:40 +0700

    feat: add function post put and delete

--- src/shared/api/network.ts
+++ src/shared/api/network.ts
@@ -1,55 +1,102 @@
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError } from "axios";
 
 const APIClient = axios.create({
-	baseURL: import.meta.env.VITE_SERVER_DOMAIN,
-	headers: {
-		Accept: 'application/json',
-		'Content-Type': 'application/json',
-	},
+  baseURL: import.meta.env.VITE_SERVER_DOMAIN,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
 
 export async function post({ path, data }: { path: string; data?: unknown }) {
-	try {
-		const response = await APIClient.post(path, data);
+  try {
+    const response = await APIClient.post(path, data);
 
-		return {
-			status: response.status,
-			data: response.data,
-		};
-	} catch (error) {
-		if (error instanceof AxiosError) {
-			if (error.response?.status === 401) {
-				window.location.href = '/';
-			}
-			return {
-				status: error.response?.status || 404,
-				error: error.response?.data,
-			};
-		}
-		return { status: 404, error: error as Error };
-	}
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 401) {
+        window.location.href = "/";
+      }
+      return {
+        status: error.response?.status || 404,
+        error: error.response?.data,
+      };
+    }
+    return { status: 404, error: error as Error };
+  }
 }
 
 export async function get(path: string) {
-	try {
-		const response = await APIClient.get(path);
-		if (response.status !== 200) throw new Error(response.data.message);
+  try {
+    const response = await APIClient.get(path);
+    if (response.status !== 200) throw new Error(response.data.message);
 
-		return {
-			status: response.status,
-			data: response.data,
-		};
-	} catch (error) {
-		if (error instanceof AxiosError) {
-			if (error.response?.status === 401) {
-				window.location.href = '/';
-			}
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 401) {
+        window.location.href = "/";
+      }
 
-			return {
-				status: error.response?.status || 404,
-				error: error.response?.data || error,
-			};
-		}
-		return { status: 404, error: error as Error };
-	}
+      return {
+        status: error.response?.status || 404,
+        error: error.response?.data || error,
+      };
+    }
+    return { status: 404, error: error as Error };
+  }
+}
+
+export async function put({ path, data }: { path: string; data?: unknown }) {
+  try {
+    const response = await APIClient.put(path, data);
+
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 401) {
+        window.location.href = "/";
+      }
+
+      return {
+        status: error.response?.status || 404,
+        error: error.response?.data,
+      };
+    }
+    return { status: 404, error: error as Error };
+  }
+}
+
+export async function deleteData(path: string) {
+  try {
+    const response = await APIClient.delete(path);
+    if (response.status !== 200) throw new Error(response.data.message);
+
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 401) {
+        window.location.href = "/";
+      }
+
+      return {
+        status: error.response?.status || 404,
+        error: error.response?.data || error,
+      };
+    }
+    return { status: 404, error: error as Error };
+  }
 }
