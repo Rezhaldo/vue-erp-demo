57b49375a7bad7534c46c4f514adda2459270dd8
kevinchilmirezhaldo <rezhaldo@gmail.com>
2025-08-10 01:44:49 +0700

    feat: configure Login

--- src/modules/authentication/pages/login/ui/LoginPage.vue
+++ src/modules/authentication/pages/login/ui/LoginPage.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="MainContent">
     <div class="login-container">
-      <h1>Login</h1>
+      <h1 style="font-size: 4ch; font-weight: 600">Login</h1>
 
       <form @submit.prevent="handleSubmit">
         <div class="form-group">
           <label for="username">Username</label>
           <input
+            class="login-input"
             id="username"
             v-model="username"
             type="text"
@@ -18,6 +19,7 @@
         <div class="form-group">
           <label for="password">Password</label>
           <input
+            class="login-input"
             id="password"
             v-model="password"
             type="password"
@@ -26,20 +28,31 @@
           />
         </div>
 
-        <button type="submit">Sign-in</button>
+        <button class="signin-button" type="submit">Sign-in</button>
       </form>
+      <div class="login-container" style="margin-top: 20px">
+        <p>
+          <strong>Test Credentials:</strong>
+          <br />
+          Username: <code>emilys</code>
+          <br />
+          Password: <code>emilyspass</code>
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 import { toast } from "vue3-toastify";
 import { login } from "../api/login";
 import "./LoginPage.scss";
 
-const username = ref<string>("");
-const password = ref<string>("");
+const router = useRouter();
+const username = ref<string>("emilys");
+const password = ref<string>("emilyspass");
 
 async function handleSubmit() {
   try {
@@ -48,6 +61,8 @@ async function handleSubmit() {
       password: password.value,
     });
     console.log("✅ Login Success", data);
+    router.push("/dashboard");
+
     // TODO: navigate to dashboard or store token
   } catch (error: any) {
     toast.error(error?.message || "Login failed ❌");
